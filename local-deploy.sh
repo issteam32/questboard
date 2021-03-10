f9c5cea632584aeab80e13a4af94b4440edc4c43
lofmana <lofmana@hotmail.com>
2021-03-11 01:38:34 +0800

    Initial commit of chatservice
    commit intitial chat service , not working yet

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,6 +3,8 @@ BYellow='\033[1;33m'
 Color_Off='\033[0m'
 
 ./user-service-build.sh
+./chat-service-build.sh
+./demo-service-build.sh
 
 echo -e "[${BYellow}DEPLOY-MESSAGE${Color_Off}] --- deploying using skaffold ---"
 
