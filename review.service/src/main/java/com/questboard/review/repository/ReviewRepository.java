f5a9c7d50cf2569bd2dab5f5f41ca03c4d7fe5cc
Matthew Liew <matthew.liew@ncs.com.sg>
2021-03-27 14:01:43 +0800

    Update list
    1. Full review service development
    2. Full unit test for review service
    3. Updated review.deployment yaml
    4. Updated skaffold in root folder

---
+++ review.service/src/main/java/com/questboard/review/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.questboard.review.repository;
+
+import com.questboard.review.entity.Review;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ReviewRepository extends ReactiveSortingRepository<Review, Integer> {
+
+    Flux<Review> findByQuestid(Integer quest_id, Pageable pageable);
+    Flux<Review> findByQuesttakerid(Integer quest_taker_id, Pageable pageable);
+}
