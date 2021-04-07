09bda061f9380886c4d55a03d15f7ee18e1af7f5
YJChan <yjtoro@hotmail.com>
2021-04-08 01:28:41 +0800

    configure travis build with maven cache

--- travis-build.sh
+++ travis-build.sh
@@ -1,16 +1,13 @@
 #deploy using skaffold
-BYellow='\033[1;33m'
-Color_Off='\033[0m'
-
-echo -e "[${BYellow}DEPLOY-MESSAGE${Color_Off}] --- running travis build ---"
+echo -e "[DEPLOY-MESSAGE] --- running travis build ---"
 
 ./init-script/user-service-build.sh
-docker build -t issteam32/user-service -f ./user.service/Dockerfile .
+docker build -t issteam32/user-service -f ./user.service/Dockerfile.prod .
 
-./init-script/quest-service-build.sh
-docker build -t issteam32/quest-service -f ./quest.service/Dockerfile .
+#./init-script/quest-service-build.sh
+#docker build -t issteam32/quest-service -f ./quest.service/Dockerfile .
+#
+#./init-script/chat-service-build.sh
+#docker build -t issteam32/chat-service -f ./ChatWebsocket/Dockerfile .
 
-./init-script/chat-service-build.sh
-docker build -t issteam32/chat-service -f ./ChatWebsocket/Dockerfile .
-
-echo -e "[${BYellow}DEPLOY-MESSAGE${Color_Off}] --- done ---"
+echo -e "[DEPLOY-MESSAGE] --- done ---"
