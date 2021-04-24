277a350394840bfb446ca25e0e6c7eebb278dbd7
YJChan <yjtoro@hotmail.com>
2021-04-24 18:17:24 +0800

    correct reward service port

--- reward.service/src/main/java/com/questboard/reward/config/SecurityConfig.java
+++ reward.service/src/main/java/com/questboard/reward/config/SecurityConfig.java
@@ -18,9 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         String jwkSetUri = Objects.requireNonNull(env.getProperty("OAUTH2SERVER_JWKURI"));
-        System.out.println("################## review env variable ###################");
+        System.out.println("################## reward env variable ###################");
         System.out.println(jwkSetUri);
-        System.out.println("################## review env variable ###################");
+        System.out.println("################## reward env variable ###################");
         http.cors()
                 .and()
                 .csrf().disable()
